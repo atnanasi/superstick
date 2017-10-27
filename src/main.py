56d95bcb5a14209107be3bb2d0e91dbdab0506bd
Atnanasi <ys2000pro@gmail.com>
2017-10-28 02:53:23 +0900

    コマンド対応

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
+import sys
+
+args = sys.argv
 
 def main():
-    print("Hello")
+    if args[1]=="start":
+        payment()
+
+    elif args[1]=="item":
+        add_item()
+
+    elif args[1]=="items":
+        print(get_items)
+
+    elif "item@" in args[1]:
+        edit_item(args[1].split("@")[1:])
+
+    else:
+        print("superstick: start,item,items,item@%n")
 
 if __name__ == '__main__':
     main()
