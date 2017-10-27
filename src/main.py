dbffc37f124b22a6b956c4ec57e02f90917aa066
Atnanasi <ys2000pro@gmail.com>
2017-10-28 02:14:34 +0900

    fiejwa

--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-import configparser
-
-goods = configparser.ConfigParser()
-goods.read('config/goods.ini', encoding="utf-8")
 
 def main():
-    print("Superstick v1.0")
-    print("0:owari 1~:subtotal")
-
-    while True:
-        c = input("> ")
-        if c=="0":
-            print("OK-Woman")
-            break
-
-        else:
-            if c in goods:
-                pass
-
+    print("Hello")
 
 if __name__ == '__main__':
     main()
